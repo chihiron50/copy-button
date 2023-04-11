437771024c267888fa17aff7c244dbaa8f7c425b
Chihiron <98100427+chihiron50@users.noreply.github.com>
2023-04-11 16:00:51 +0900

    Add files via upload

---
+++ コピーさせるボタン/index.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('copy_button');
+button.addEventListener('click',()=>{
+alert('copy now')
+navigator.clipboard.writeText('クルンテープ・マハーナコーン・アモーン・ラッタナコーシン・マヒンタラー・ユッタヤーマ・ハーディ・ロック・ポップ・ノッパラット・ラーチャタニー・ブリーロム・ウドムラー・チャニウェート・マハー・サターン・アモーン・ピマーン・アワターン・サティット・サッカタッティヤ・ウィサヌカムプラシット')
+
+})
+
+button.addEventListener('touchstart',()=>{
+    alert('copy now')
+    navigator.clipboard.writeText('クルンテープ・マハーナコーン・アモーン・ラッタナコーシン・マヒンタラー・ユッタヤーマ・ハーディ・ロック・ポップ・ノッパラット・ラーチャタニー・ブリーロム・ウドムラー・チャニウェート・マハー・サターン・アモーン・ピマーン・アワターン・サティット・サッカタッティヤ・ウィサヌカムプラシット')
+    
+    })
